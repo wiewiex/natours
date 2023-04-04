e278e636e7df71fe75ba2b9f76c3ddfdf082b0a0
Jacek Pietrzak <wiewiex@gmail.com>
2023-04-04 12:01:47 +0200

    error controller

--- models/tourModel.js
+++ models/tourModel.js
@@ -9,7 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name!'],
       unique: true,
       trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      maxLength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have more or equal then 10 characters'],
       // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
