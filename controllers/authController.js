a30a5e6366419cecef5f1ee92a32d617bb2a03a5
Jacek Pietrzak <wiewiex@gmail.com>
2023-04-17 15:34:50 +0200

    send token via http cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
-const crypto = require('crypto');
 
 const signToken = (id) => {
   return jwt.sign(
@@ -18,6 +18,19 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
