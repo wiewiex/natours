79dd48b7baef97e79cdceb203964d71ce0a8277e
Jacek Pietrzak <wiewiex@gmail.com>
2023-04-17 15:56:03 +0200

    security -add helmet to global middlewares

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
@@ -10,9 +11,17 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logger
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//Limit request from same IP
 
 const limiter = rateLimit({
   max: 100,
@@ -22,9 +31,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser - reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
