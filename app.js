4c45d8ae70c87fe4dd5e60a2f89abcc78cca0925
Jacek Pietrzak <wiewiex@gmail.com>
2023-04-26 17:16:54 +0200

    fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -55,6 +56,8 @@ app.use('/api', limiter);
 
 // Body parser - reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+app.use(cookieParser());
 
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
--- controllers/authController.js
+++ controllers/authController.js
@@ -19,12 +19,12 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  cookieOptions.res.cookie('jwt', token, {
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto'),
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   });
 
   user.password = undefined;
