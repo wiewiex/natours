81289b924f44d44d086eb9cb39d799f94097c285
Jacek Pietrzak <wiewiex@gmail.com>
2023-04-26 12:43:15 +0200

    add booking handlers

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-// const handlerFactory = require('./handlerFactory');
-// const AppError = require('../utils/appError');
+const handlerFactory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
@@ -10,7 +10,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card', 'p24'],
     mode: 'payment',
-    success_url: `${process.env.FRONTEND_URL}/`,
+    success_url: `${process.env.FRONTEND_URL}/api/v1/bookings/success?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${process.env.FRONTEND_URL}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -18,7 +18,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         quantity: 1,
         price_data: {
-          currency: 'usd',
+          currency: 'pln',
           unit_amount: tour.price * 100,
           product_data: {
             name: `${tour.name} Tour`,
@@ -35,3 +35,24 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect('https://jacekpietrzak.pl');
+});
+
+exports.getMyBookings = catchAsync(async (req, res, next) => {
+  const bookings = await Booking.find({ user: req.user.id });
+
+  res.status(200).json({
+    status: 'success',
+    data: bookings,
+  });
+});
+
+exports.getOneBooking = handlerFactory.getOne(Booking);
+exports.getAllBookings = handlerFactory.getAll(Booking);
+exports.deleteOneBooking = handlerFactory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,12 +4,40 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(authController.protect);
-
 router.get(
   '/checkout-session/:tourId',
   authController.protect,
   bookingController.getCheckoutSession
+);
+
+router.get('/success', bookingController.createBookingCheckout);
+
+router.get(
+  '/my-bookings',
+  authController.protect,
+  authController.restrictTo('user'),
+  bookingController.getMyBookings
+);
+
+router.get(
+  '/',
+  authController.protect,
+  authController.restrictTo('admin'),
+  bookingController.getAllBookings
+);
+
+router.get(
+  '/:id',
+  authController.protect,
+  authController.restrictTo('admin'),
+  bookingController.getOneBooking
+);
+
+router.delete(
+  '/:id',
+  authController.protect,
+  authController.restrictTo('admin'),
+  bookingController.deleteOneBooking
 );
 
 module.exports = router;
